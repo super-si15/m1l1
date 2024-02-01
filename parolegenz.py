7b17947018eeef13d973632b27a75fd8e07fa0df
super-si15 <158501253+super-si15@users.noreply.github.com>
2024-02-01 18:32:52 +0100

    Create parolegenz.py

---
+++ parolegenz.py
@@ -0,0 +1,7 @@
+dizionario = {"LOL - una risposta a qualcosa di divertente
+"CRINGE - qualcosa di strano o imbarazzante
+"SHEESH - leggera disapprovazione
+"CREEPY - spaventoso, inquietante
+"PARA - preoccuparsi per qualcosa, paranoiarsi"}
+
+if parola in dizionario.keys():
